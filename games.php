7ed4112b56de04b93999d53cc1d52a20f54e305c
Bordes Remy <remy.bordes@ynov.com>
2026-03-03 08:55:05 +0100

    feat: adding front of page

---
+++ games.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Aetheria - Détail</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="games.css">
+</head>
+
+<body>
+
+<header>
+    <div class="logo">
+        <img src="Image/logo.png" alt="logo">
+        Aetheria
+    </div>
+    <nav>
+        <a href="index.php">Accueil</a>
+        <a href="#">Connexion/Inscription</a>
+    </nav>
+</header>
+
+<section class="container">
+
+    <div class="game-card">
+
+        <div class="game-image">
+            <img src="Image/ff1.jpg" alt="Final Fantasy 1">
+        </div>
+
+        <div class="game-info">
+            <h2>Final Fantasy 1</h2>
+            <p><strong>Date :</strong> 12/12/2020</p>
+            <p><strong>Editeurs :</strong> Square Enix</p>
+            <p>
+                <strong>Description :</strong> Professionalis diligens et motivatus sum,
+                magna facultate accommodandi et sensu ordinis praeditus.
+                Per experientias meas in campo, artes excolui atque animum
+                cooperandi et autonomiam confirmavi.
+            </p>
+
+            <h3>Succès :</h3>
+
+            <div class="achievements">
+                <div class="achievement">
+                    <img src="Image/icon1.png">
+                    <p>Veuillez<br>Acheter le jeu</p>
+                </div>
+
+                <div class="achievement">
+                    <img src="Image/icon2.png">
+                    <p>Veuillez<br>Acheter le jeu</p>
+                </div>
+
+                <div class="achievement">
+                    <img src="Image/icon3.png">
+                    <p>Veuillez<br>Acheter le jeu</p>
+                </div>
+
+                <div class="achievement">
+                    <img src="Image/icon4.png">
+                    <p>Veuillez<br>Acheter le jeu</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="price-box">
+            <p class="price">Prix : $$$</p>
+            <button>Acheter</button>
+        </div>
+    </div>
+</section>
+
+<footer>
+    2026 - Project Etudiants
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Aetheria</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+<header>
+    <div class="logo">
+        <img src="Images\logo.png" alt="logo">
+        Aetheria
+    </div>
+    <nav>
+        <a href="games.php">Accueil</a>
+        <a href="#">Connexion/Inscription</a>
+    </nav>
+</header>
+
+<h1 class="main-title">Jeu de la Saga :</h1>
+
+<section class="grid">
+
+    <div class="card">
+        <img src="Images/final_fantasy.jpg">
+        <div class="card-content">
+            <h3>Final Fantasy 1</h3>
+            <p>Premier jeu de la célèbre saga ....</p>
+            <div class="price">Prix : $</div>
+        </div>
+        <button>Acheter</button>
+    </div>
+
+    <div class="card">
+        <img src="Images/final_fantasy.jpg">
+        <div class="card-content">
+            <h3>Final Fantasy 3</h3>
+            <p>Troisième jeu de la célèbre saga ....</p>
+            <div class="price">Prix : $$$</div>
+        </div>
+        <button>Acheter</button>
+    </div>
+
+    <div class="card">
+        <img src="Images/final_fantasy_ii.jpg">
+        <div class="card-content">
+            <h3>Final Fantasy 2</h3>
+            <p>Deuxième jeu de la célèbre saga ....</p>
+            <div class="price">Prix : $$</div>
+        </div>
+        <button>Acheter</button>
+    </div>
+
+    <div class="card">
+        <img src="Images/final_fantasy_ii.jpg">
+        <div class="card-content">
+            <h3>Final Fantasy 4</h3>
+            <p>Quatrième jeu de la célèbre saga ....</p>
+            <div class="price">Prix : $$$$</div>
+        </div>
+        <button>Acheter</button>
+    </div>
+
+</section>
+
+<footer>
+    2026 - Project Etudiants
+</footer>
+
+</body>
+</html>
